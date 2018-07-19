95114745660bdc91ec3ea30e383da139ff6ecb7f
hkyo89 <hkyo89@yahoo.co.jp>
2018-07-19 23:57:16 +0700

    fixed root path

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const registeredModules = ['base', 'containers', 'services', 'modules', 'compone
 path.basepath = {}
 
 registeredModules.forEach((module) => {
-  path.basepath[module] = (_path) => path.resolve(__dirname, '../../', module !== registeredModules[0] ? module : '', _path || '')
+  const rootPath = '../../..'
+  path.basepath[module] = (_path) => path.resolve(__dirname, rootPath, module !== registeredModules[0] ? module : '', _path || '')
 })
 
 module.exports = path
